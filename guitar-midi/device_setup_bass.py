e09cd0d45fa1a1f2b4ea5f014062ca5b2aaa72af
Laurynas Lubys <20045155+laurynaslubys@users.noreply.github.com>
2024-06-19 13:54:36 +0300

    Port guitar controller to esp32 (#41)

--- guitar-midi/device_setup_bass.py
+++
@@ -1,8 +0,0 @@
-Import("env")
-
-board_config = env.BoardConfig()
-board_config.update("build.hwids", [
-  ["0x27B8", "0x01ED"]
-])
-board_config.update("build.usb_product", "Bass Controller")
-board_config.update("vendor", "Wix.com")
